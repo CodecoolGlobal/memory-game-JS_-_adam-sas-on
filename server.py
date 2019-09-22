087fe66ca73c446b738a8cca058664bf4b92953d
Adam Sz <adamsz303@gmail.com>
2019-09-22 22:41:42 +0200

    Add controller for new game

--- server.py
+++ server.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html', form=True)
+
+@app.route('/play-game', methods=['POST', 'GET'])
+def play():
+    if request.method == 'POST':
+        try:
+            memo_count = int(request.form['memos'])
+        except:
+            memo_count = 12
+
+        return render_template('index.html', form=True, memo_count=memo_count*2, menu_hide=True)
+
+    return render_template('index.html', form=True, menu_hide=True)
+#
 
 # https://codepen.io/natewiley/pen/HBrbL;
 
